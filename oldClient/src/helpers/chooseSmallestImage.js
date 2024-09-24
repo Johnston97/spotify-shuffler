c62cb8f768ff324516ce32b855849d617021baac
Luke Johnston <lukejohnston97@live.com>
2024-09-24 12:14:04 +0100

    Delete oldClient directory
    Delete server/.env

--- oldClient/src/helpers/chooseSmallestImage.js
+++
@@ -1,6 +0,0 @@
-export default function chooseSmallestImage(images) {
-  return images.reduce((smallest, image) => {
-    if (image.height < smallest.height) return image;
-    return smallest;
-  }, images[0]);
-}
